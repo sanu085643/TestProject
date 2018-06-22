152b4cc6ea1af5c270a0dcee28936f89bb58d2cb
Sanu_Gupta <Sanu_Gupta@MYSHEZ238480D.ad.infosys.com>
2018-06-22 14:32:20 +0530

    Added class Test.java

---
+++ TestGit/src/Test.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Sanu_Gupta
+ *
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
